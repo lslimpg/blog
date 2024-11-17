67e4c619d1e969fc89a263f66df0e5230a9d4b26
Li-Sha Lim <limxx518@gmail.com>
2024-11-17 15:15:00 -0800

    fix pre-commit linting
    Signed-off-by: Li-Sha Lim <limxx518@gmail.com>

--- functions/api/views/[[post]].js
+++ functions/api/views/[[post]].js
@@ -2,18 +2,17 @@ function parseRequestCookie(req) {
   let cookieObj = {};
   let tokens, cookie;
 
-  if (!req.headers.has('cookie'))
-    return null;
+  if (!req.headers.has("cookie")) return null;
 
-  cookie = req.headers.get('cookie');
+  cookie = req.headers.get("cookie");
   tokens = cookie.split(/=|;/);
 
-  if ((tokens.length % 2) !== 0) {
+  if (tokens.length % 2 !== 0) {
     console.warn(`Token length not even: ${tokens.length}`);
   }
 
-  for (let i = 0; i < (tokens.length / 2); i++) {
-    cookieObj[tokens[(i * 2)].trim()] = JSON.parse(tokens[(i * 2) + 1].trim());
+  for (let i = 0; i < tokens.length / 2; i++) {
+    cookieObj[tokens[i * 2].trim()] = JSON.parse(tokens[i * 2 + 1].trim());
   }
 
   return cookieObj;
@@ -33,30 +32,34 @@ async function updatePageViewThruKVCookieMethod(ctx) {
   const env = ctx.env;
   const params = ctx.params.post;
   const post = params[params.length - 1];
-  let cache, visited = null;
+  let cache,
+    visited = null;
 
   cache = parseRequestCookie(ctx.request);
-  if (cache && Object.hasOwn(cache, 'visited')) {
-    visited = new Set(cache['visited']);
+  if (cache && Object.hasOwn(cache, "visited")) {
+    visited = new Set(cache["visited"]);
     if (visited.has(`${post}`))
       return new Response(`Visited previously, returning...`, { status: 200 });
   }
   visited = visited || new Set();
   visited.add(`${post}`);
   const cookieHeader = new Headers({
-    'Set-Cookie': "visited=" + `${JSON.stringify(Array.from(visited.values()))}`,
-  })
-  let count = await env.BLOG_PAGE_VIEWS.get(`${post}`) || '0';
-  const result = await env.BLOG_PAGE_VIEWS.put(`${post}`, String(Number(count) + 1));
-  return new Response(`Updated count to ${count + 1}`, { status: 200, headers: cookieHeader });
+    "Set-Cookie":
+      "visited=" + `${JSON.stringify(Array.from(visited.values()))}`,
+  });
+  let count = (await env.BLOG_PAGE_VIEWS.get(`${post}`)) || "0";
+  await env.BLOG_PAGE_VIEWS.put(`${post}`, String(Number(count) + 1));
+  return new Response(`Updated count to ${count + 1}`, {
+    status: 200,
+    headers: cookieHeader,
+  });
 }
-
 
 function flattenObject(obj) {
   const flattened = {};
 
-  Object.keys(obj).forEach((key) => {
-    if (typeof obj[key] === 'object' && obj[key] !== null) {
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === "object" && obj[key] !== null) {
       Object.assign(flattened, flattenObject(obj[key]));
     } else {
       flattened[key] = obj[key];
@@ -75,47 +78,51 @@ async function queryDB(ctx) {
     FROM Page_Views
     WHERE post = ?1
   `;
-  const total = await ctx.env.DB.prepare(query).bind(`${post}`).first("totalViews");
+  const total = await ctx.env.DB.prepare(query)
+    .bind(`${post}`)
+    .first("totalViews");
   return total;
 }
 
 async function updateDB(insert, totalViews, ctx) {
   const params = ctx.params.post;
   const post = params[params.length - 1];
-  const lastUpdated = new Date().toISOString().split('T')[0];
-  const query =  insert ? 
-  `INSERT INTO Page_Views (post, totalViews, lastUpdated) VALUES (?1, ?2, ?3)` :
-  `
+  const lastUpdated = new Date().toISOString().split("T")[0];
+  const query = insert
+    ? `INSERT INTO Page_Views (post, totalViews, lastUpdated) VALUES (?1, ?2, ?3)`
+    : `
     UPDATE Page_Views
     SET totalViews = ?2, lastUpdated = ?3
     WHERE post = ?1
   `;
   try {
-    const { success } = await ctx.env.DB.prepare(query).bind(`${post}`, totalViews, lastUpdated).run();
+    const { success } = await ctx.env.DB.prepare(query)
+      .bind(`${post}`, totalViews, lastUpdated)
+      .run();
     return success;
   } catch (e) {
     console.error(e.message);
     return null;
   }
-  
 }
 
 async function getPageViewThruCFAnalytics(ctx) {
   const params = ctx.params.post;
-  const today = new Date(), yesterday = new Date(today.getTime() - (24 * 60 * 60 * 1000));
+  const today = new Date(),
+    yesterday = new Date(today.getTime() - 24 * 60 * 60 * 1000);
   let pathUrl = "/";
 
   for (let p of params) {
     pathUrl += p + "/";
   }
 
-  const resp = await fetch('https://api.cloudflare.com/client/v4/graphql', {
-    method: 'POST',
+  const resp = await fetch("https://api.cloudflare.com/client/v4/graphql", {
+    method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'X-AUTH-EMAIL': `${ctx.env.X_AUTH_EMAIL}`,
-      'AUTHORIZATION': `${ctx.env.ACC_ANALYTICS_TOKEN}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-AUTH-EMAIL": `${ctx.env.X_AUTH_EMAIL}`,
+      AUTHORIZATION: `${ctx.env.ACC_ANALYTICS_TOKEN}`,
     },
     body: JSON.stringify({
       query: `query GetRumAnalyticsTopNs {
@@ -128,20 +135,20 @@ async function getPageViewThruCFAnalytics(ctx) {
   }
 }`,
       variables: {
-        "accountTag": `${ctx.env.ACCOUNT_TAG}`,
-        "filter": {
-          "AND": [
+        accountTag: `${ctx.env.ACCOUNT_TAG}`,
+        filter: {
+          AND: [
             {
-              "datetime_geq": `${yesterday.toISOString()}`,
-              "datetime_leq": `${today.toISOString()}`
+              datetime_geq: `${yesterday.toISOString()}`,
+              datetime_leq: `${today.toISOString()}`,
             },
             {
-              "bot": 0
+              bot: 0,
             },
             {
-              "requestPath": `${pathUrl}`
-            }
-          ]
+              requestPath: `${pathUrl}`,
+            },
+          ],
         },
       },
     }),
@@ -158,10 +165,13 @@ export async function onRequestGet(ctx) {
     if (errors) {
       return new Response(null, { status: 500, statusText: errors });
     }
-    let totalViews = await queryDB(ctx) || 0;
+    let totalViews = (await queryDB(ctx)) || 0;
     res = await updateDB(totalViews === null, (totalViews += count), ctx);
     if (!res) {
-      return new Response(null, { status: 500, statusText: "DB update failed" });
+      return new Response(null, {
+        status: 500,
+        statusText: "DB update failed",
+      });
     }
     // const count = await getPageViewThruKVCookieMethod(ctx);
     // if (count === null) {
--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,17 +1,18 @@
-
 export interface Props {
   src: string;
 }
-export default function Media({src} : Props) {
+export default function Media({ src }: Props) {
   return (
     <>
-      <div style={{display: 'flex', height: '518px'}}>
+      <div style={{ display: "flex", height: "518px" }}>
         <iframe
-          allow="fullscreen" loading="lazy"
-          width='100%' height= '100%' src={src}>
-        </iframe>
-    </div>
+          allow="fullscreen"
+          loading="lazy"
+          width="100%"
+          height="100%"
+          src={src}
+        ></iframe>
+      </div>
     </>
-
-  )
+  );
 }
